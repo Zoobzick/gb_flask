8aeb13ff3fb3e976c0967bb72fee354fb6c66ff6
Troover <murdoc-94@mail.ru>
2023-03-16 19:47:17 +0300

    lesson_3

---
+++ blog/auth/views.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import logout_user, LoginManager, login_user
+from werkzeug.security import check_password_hash
+
+from blog.models.models import Users
+from flask_login import login_required
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+
+auth = Blueprint('auth', __name__, static_folder='../static')
+
+@auth.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('auth/login.html')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = Users.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Check your input details')
+        return redirect(url_for('.login'))
+    login_user(user)
+    return redirect(url_for('auth.profile', pk=user.id))
+
+
+@auth.route('/logout')
+@login_manager.user_loader
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('.login'))
+
+
+@auth.route("/secret/")
+@login_required
+def secret_view():
+    return "Super secret data"
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,23 @@
+from werkzeug.security import generate_password_hash
+
 from blog.app import create_app
+from blog.models.models import db, Users
 
 app = create_app()
+
+@app.cli.command('init-db')
+def init_db():
+    with app.app_context():
+        db.create_all()
+        print('DB created')
+
+
+@app.cli.command('create-users')
+def create_users():
+    with app.app_context():
+        admin = Users(username='root', email='root@email.com', is_staff=True, password=generate_password_hash('test'))
+        client = Users(username='trv', email='trv@mail.ru', is_staff=False, password=generate_password_hash('test1'))
+        db.session.add(admin)
+        db.session.add(client)
+        db.session.commit()
+        print('Done, users created')
