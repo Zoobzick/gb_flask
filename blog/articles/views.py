f119c79c56ec7ecd066f179181f3b3e0856b7b2d
Troover <94111543+Zoobzick@users.noreply.github.com>
2023-04-02 19:11:26 +0300

    lesson_7 done (#8)

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, request, current_app, redirect, url_for
 from flask_login import login_required, current_user
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import joinedload
 from werkzeug.exceptions import NotFound
 
 from blog.forms.article import CreateArticleForm
+from blog.models import Tag
 from blog.models.models import Articles, db, Author
 
 article = Blueprint('articles', __name__, url_prefix='/articles', static_folder='../static')
@@ -17,7 +19,7 @@ def article_list():
 
 @article.route("/<int:article_id>/", endpoint="details")
 def article_details(article_id):
-    article = Articles.query.filter_by(id=article_id).one_or_none()
+    article = Articles.query.filter_by(id=article_id).options(joinedload(Articles.tags)).one_or_none()
     if article is None:
         raise NotFound
     return render_template("articles/details.html", article=article)
@@ -28,8 +30,14 @@ def article_details(article_id):
 def create_article():
     error = None
     form = CreateArticleForm(request.form)
+    form.tags.choices = [(tag.id, tag.name ) for tag in Tag.query.order_by("name")]
     if request.method == "POST" and form.validate_on_submit():
         article = Articles(title=form.title.data.strip(), text=form.text.data)
+        if form.tags.data :
+            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
+            for tag in selected_tags:
+                article.tags.append(tag)
+
         db.session.add(article)
         if current_user.author:
             article.author = current_user.author
---
+++ blog/models/__init__.py
@@ -0,0 +1,9 @@
+from blog.models.tag import Tag
+from blog.models.models import Users, Author, Articles
+
+__all__ = [
+    "Users",
+    "Author",
+    "Articles",
+    "Tag"
+]
