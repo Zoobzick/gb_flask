39f6d8cb2c285dd7442f1b92ef8bc9cb67d12425
Troover <murdoc-94@mail.ru>
2023-04-11 16:45:38 +0300

    config db url changed

--- blog/config.py
+++ blog/config.py
@@ -3,7 +3,7 @@
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://trv:password@localhost:5432/blog"
+    SQLALCHEMY_DATABASE_URI = "mysql://root@https://gb-flask-ycuz.onrender.com:3306/blog"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'a%8g)$xp+&2tq15b#=#(-96a6b!4i$0$js_1*m#e7hrqq=ik9='
     WTF_CSRF_ENABLED = True
@@ -13,7 +13,7 @@ class BaseConfig(object):
 
 
 class DevConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = "mysql://root:vlad72kbhjqrjJ!@localhost:3306/blog"
+    SQLALCHEMY_DATABASE_URI = "mysql://root@https://gb-flask-ycuz.onrender.com:3306/blog"
     DEBUG = True
 
 
